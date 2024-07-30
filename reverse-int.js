ca79a476a253cbf7fdaa9631dd2d0d413ca5cab3
1ster1ka <temhbl@tut.by>
2024-07-30 09:02:10 +0300

    feat: add code

---
+++ reverse-int.js
@@ -0,0 +1,7 @@
+    let arr=(""+numder).split('').map(Number)
+    let rev_arr=arr.reverse()
+    let new_number=''
+    for (i=0; i<arr.length;i++){
+        new_number+=rev_arr[i]
+    }
+    return new_number
